68b3f67be74ce552cf0d75cd27b7caf34007b9dc
ejioo-iscteiulpt <edhimar23@gmail.com>
2017-03-16 22:36:47 +0000

    so para treinar antes de fazer o video

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -27,6 +27,8 @@ import mt.filter.AnalyticsFilter;
  * @author Group 78
  *
  */
+
+// não se assustem, so estou a experimentar
 public class MicroServer implements MicroTraderServer {
 	
 	public static void main(String[] args) {
